b8b15ce279725b97c17970a257adf36358431891
Phumipat Jitreephot <Phumipat30439@gmail.com>
2025-03-06 18:34:34 +0700

    delete mathplotlib

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
